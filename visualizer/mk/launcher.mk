7eff164d5bfa26bf396ff68160be15bce7424b3a
tyamada <tyamada@roboken.esys.tsukuba.ac.jp>
2013-08-12 17:29:13 +0900

    update visualizer makefile

---
+++ visualizer/mk/launcher.mk
@@ -0,0 +1,15 @@
+
+#launcher shell script name
+LAUNCHER		:=launcher
+
+#lanch option
+LAUNCH_CMD		:=./$(RELEASE_DIR)$(TARGET)
+
+#lanch option
+LAUNCH_CONFIG	:=$(PWD)/opsm-particle-evaluator.conf
+
+#launch command
+LAUNCH_SCRIPT	:=./$(RELEASE_DIR)$(TARGET)
+
+#shell command interpreter
+SHELL_INTRP		:=/bin/bash
---
+++ visualizer/mk/subdir.mk
@@ -0,0 +1,3 @@
+
+RELEASE_DIR:=release/
+
