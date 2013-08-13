b51655e288c31a3d2744ff55cc04317cf4bb9650
tyamada <tyamada@roboken.esys.tsukuba.ac.jp>
2013-08-13 21:17:58 +0900

    Squashed 'ls-coordinate-converter/' changes from dd07a27..c6298ff
    c6298ff update makefile
    
    git-subtree-dir: ls-coordinate-converter
    git-subtree-split: c6298ffbd70ae62824355a66f5d68ae67eee775f

--- mk/launcher.mk
+++ mk/launcher.mk
@@ -3,10 +3,33 @@
 LAUNCHER		:=launcher
 
 #lanch option
-LAUNCH_OPTION	:=
+LAUNCH_CMD		:=./$(RELEASE_DIR)$(TARGET)
+
+#launcher shell script name
+LAUNCHER_INC		:=launcher.opt
+
+#lanch option tag
+LAUNCH_OPTION_TAG	:=OPTION
+
+#lanch option
+LAUNCH_OPTION		:=
 
 #launch command
-LAUNCH_CMD		:=./$(RELEASE_DIR)$(TARGET) $(LAUNCH_OPTION) "$$"@
+LAUNCH_SCRIPT = \
+echo $(LAUNCH_CMD) \$${$(LAUNCH_OPTION_TAG)} \$$@\n\
+$(LAUNCH_CMD) \$${$(LAUNCH_OPTION_TAG)} \$$@
 
 #shell command interpreter
-SHELL_INTRP		:=/bin/bash
+SHELL_INTRP			:=/bin/bash
+
+define make-launcher
+	@$(shell) echo -e "#!$(SHELL_INTRP)" > $(LAUNCHER)
+	@$(shell) echo -e ". $(LAUNCHER_INC)" >> $(LAUNCHER)
+	@$(shell) echo -e "$(LAUNCH_SCRIPT)" >> $(LAUNCHER)
+	@chmod +x $(LAUNCHER)
+	@$(shell) echo -e "create launcher"
+endef
+
+define clean-launcher
+	$(REMOVE) $(LAUNCHER)
+endef
